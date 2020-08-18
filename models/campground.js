1a975c910b8c827d81694d1c67df30bec8b0e183
Eneda <enedaxhambazi@gmail.com>
2020-08-18 23:07:06 +0000

    initial deploy commit

---
+++ models/campground.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB!'))
+.catch(error => console.log(error.message));
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	price: String,
+	image: String,
+	description: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	},
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
+})
+
+module.exports = mongoose.model("Campground", campgroundSchema);
